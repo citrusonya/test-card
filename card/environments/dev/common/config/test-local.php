40484a411cf03acb4156fe8f8af4bc0089393cf1
Alexandra <36158558+citrusonya@users.noreply.github.com>
2021-11-21 16:24:14 +0300

    Настройка подключения к БД

--- card/environments/dev/common/config/test-local.php
+++ card/environments/dev/common/config/test-local.php
@@ -3,7 +3,7 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
+            'dsn' => 'pgsql:host=pgsql;port=5432;dbname=card',
         ],
     ],
 ];
--- card/environments/prod/common/config/main-local.php
+++ card/environments/prod/common/config/main-local.php
@@ -2,15 +2,23 @@
 
 return [
     'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
+        'db'     => [
+            'class'               => 'yii\db\Connection',
+            'dsn'                 => 'pgsql:host=pgsql;port=5432;dbname=card',
+            'username'            => 'card',
+            'password'            => 'cardLocal',
+            'charset'             => 'utf8',
+            'enableSchemaCache'   => true,
+            'schemaCacheDuration' => 3600,
+            'schemaMap'           => [
+                'pgsql' => [
+                    'class'         => 'yii\db\pgsql\Schema',
+                    'defaultSchema' => 'main'
+                ]
+            ],
         ],
         'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
+            'class'    => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
         ],
     ],
